b33814b67405afd1a695e02a252c8b1dddaae30d
eduardomota <publish@eduardomota.xyz>
2021-08-12 15:25:58 +0100

    Rewritten load contents
    rewritten load contents

--- app/js/renderer/loadContents.js
+++ app/js/renderer/loadContents.js
@@ -1,11 +1,125 @@
 // jshint esversion: 8
 
 /*
-  loadHtml (self-executing)
+  loadContents (self-executed)
+    Loads application contents
+ */
+(async function loadContents() {
+  loadHtml();
+})();
+
+/*
+  Load language only when document is ready
+ */
+$(document).ready(() => {
+  loadLanguage();
+  return;
+});
+
+/*
+  loadLanguage (self-executing)
+    Loads the adequate language onto the app, defaults to english
+ */
+function loadLanguage() {
+  var language = detectLanguage();
+
+  ipcRenderer.send('language:update', language);
+
+  var languageFile = getLanguageFile(language);
+
+  for (var property in languageFile)
+    if (languageFile.hasOwnProperty(property))
+      $(`#${property}`).text(languageFile[property]);
+
+  return;
+}
+
+/*
+  getLanguageFile
+    Gets the langauge file
+ */
+function getLanguageFile(language) {
+  var languagePath = getLanguagePathObject();
+  languagePath = { ...languagePath,
+    language: language
+  };
+  var fullLanguagePath = getLanguageFullPathVar(languagePath);
+
+  var jsonFile = null;
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': `${fullLanguagePath}`,
+    'dataType': "json",
+    'success': function(contents) {
+      jsonFile = contents;
+    }
+  });
+
+  return jsonFile;
+}
+
+/*
+  getLanguagePathObject
+ */
+function getLanguagePathObject() {
+  var languagePathObject = {
+    path: '../js/locale/',
+    extension: '.json'
+  };
+
+  return languagePathObject;
+}
+
+/*
+  getLanguageFullPathVar
+ */
+function getLanguageFullPathVar(languagePath) {
+  var languageFullPathVar = `${languagePath.path}${languagePath.language}${languagePath.extension}`;
+
+  return languageFullPathVar;
+}
+
+/*
+  loadHtml
     Loads HTML files inside the renderer
  */
-(async function loadHtml() {
-  var base = {
+function loadHtml() {
+  var base = getHtmlPathObject();
+  var fullPath = getHtmlFullPathObject(base);
+
+  // Dynamically load through property, element id
+  for (var property in fullPath)
+    if (fullPath.hasOwnProperty(property))
+      $(`#${property}`).load(fullPath[property]);
+
+  return;
+}
+
+/*
+  getFullPathObject
+    Compiles/gets full path object
+ */
+function getHtmlFullPathObject(baseVar) {
+  var {
+    subfolder,
+    filenames
+  } = baseVar;
+
+  var fullPathObject = {
+    navTop: `${baseVar.path}${subfolder.nav}${filenames.navTop}`,
+    p2Container: `${baseVar.path}${subfolder.tabs}${filenames.p2Container}`
+  };
+
+  return fullPathObject;
+}
+
+/*
+  getPathObject
+    Gets path object for navigation files
+ */
+function getHtmlPathObject() {
+  var pathObject = {
     path: './',
     subfolder: {
       nav: 'navigation/',
@@ -16,71 +130,14 @@
       p2Container: 'p2.html'
     }
   };
-  var {
-    subfolder,
-    filenames
-  } = base;
 
-  var fullPath = {
-    navTop: `${base.path}${subfolder.nav}${filenames.navTop}`,
-    p2Container: `${base.path}${subfolder.tabs}${filenames.p2Container}`
-  };
-
-  // Dynamically load, property
-  for (var property in fullPath) {
-    if (fullPath.hasOwnProperty(property)) $(`#${property}`).load(fullPath[property]);
-  }
-
-  return;
-})();
+  return pathObject;
+}
 
 /*
-  Load language only when document is ready
+  detectLanguage
+    Get user language
  */
-$(document).ready(() => {
-  /*
-    loadLanguage (self-executing)
-      Loads the adequate language onto the app, defaults to english
-   */
-  (async function loadLanguage() {
-    var basePath = '../js/locale/',
-      language = detectLanguage(),
-      extension = '.json';
-
-    ipcRenderer.send('language:update', language);
-
-    var languageFile = (function() {
-      var jsonFile = null;
-      $.ajax({
-        'async': false,
-        'global': false,
-        'url': `${basePath}${language}${extension}`,
-        'dataType': "json",
-        'success': function(contents) {
-          jsonFile = contents;
-        }
-      });
-
-      return jsonFile;
-    })();
-
-    for (var property in languageFile) {
-      if (languageFile.hasOwnProperty(property)) {
-        $(`#${property}`).text(languageFile[property]);
-      }
-    }
-    //console.log(json);
-
-  })();
-
-  /*
-    detectLanguage
-      Get user language
-   */
-  function detectLanguage() {
-    return navigator.language;
-  }
-
-});
-
-//loadHtml();
+function detectLanguage() {
+  return navigator.language;
+}
