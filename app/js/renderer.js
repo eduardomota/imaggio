cc5e8f8f0d79c7b304910c85e0faf1fc45110e5b
eduardomota <publish@eduardomota.xyz>
2021-08-12 18:21:29 +0100

    Update renderer.js

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -38,16 +38,14 @@ $(document).ready(() => {
     Open file/folder button
    */
   $('#S2OneButton').click(function() {
-    if (options.conversionType === 'pdfmetaclone') {
+    if (options.conversionType === 'pdfmetaclone')
       ipcRenderer.send('convertpdf:input.file.clone');
-      return;
-    }
+
     if ($('#SOProcessFolderTickbox').is(':checked')) {
       ipcRenderer.send('convertpdf:input.folder');
     } else {
       ipcRenderer.send('convertpdf:input.file');
     }
-    return;
   });
 
   /*
@@ -55,7 +53,6 @@ $(document).ready(() => {
    */
   $('#S3Button').click(function() {
     ipcRenderer.send('convertpdf:input.file');
-    return;
   });
 
   /*
@@ -63,14 +60,14 @@ $(document).ready(() => {
    */
   $('#p2killprocess').click(function() {
     ipcRenderer.send('convertpdf:killprocess');
-    return;
   });
 
   /*
     Process folder tickbox
    */
   $('#SOProcessFolderTickbox').click(function() {
-    if (options.conversionType === 'pdfmetaclone') return;
+    if (options.conversionType === 'pdfmetaclone')
+      return;
 
     if ($(this).is(':checked')) {
       $('#stepTwoLabel').addClass('is-hidden');
@@ -95,7 +92,6 @@ $(document).ready(() => {
   ipcRenderer.on('convertpdf:input.file.converting', function(event) {
     $('#Start').addClass('is-hidden');
     $('#Processing').removeClass('is-hidden');
-    return;
   });
 
   /*
@@ -105,7 +101,6 @@ $(document).ready(() => {
     $('#Start').addClass('is-hidden');
     $('#MassProcessing').removeClass('is-hidden');
     $('#MPLabel').text(progress);
-    return;
   });
 
   /*
@@ -117,12 +112,13 @@ $(document).ready(() => {
     } else {
       $('#Processing').addClass('is-hidden');
     }
+
     $('#Start').removeClass('is-hidden');
     $('#ProcessingFinished').removeClass('is-hidden');
+
     setTimeout(() => {
       $('#ProcessingFinished').addClass('is-hidden');
     }, 2500);
-    return;
   });
 
   /*
@@ -135,7 +131,6 @@ $(document).ready(() => {
     setTimeout(() => {
       $('#ProcessingFailed').addClass('is-hidden');
     }, 2500);
-    return;
   });
 
   /*
@@ -157,7 +152,8 @@ $(document).ready(() => {
 
     holder.ondrop = function(event) {
       event.preventDefault();
-      if (options.conversionType === 'pdfmetaclone') return;
+      if (options.conversionType === 'pdfmetaclone')
+        return;
       for (let f of event.dataTransfer.files) {
         if ($('#SOProcessFolderTickbox').is(':checked')) {
           ipcRenderer.send('convertpdf:drag.folder', f.path);
@@ -174,7 +170,8 @@ $(document).ready(() => {
    */
   $('#p2Container').on('drop', function(event) {
     event.preventDefault();
-    if (options.conversionType === 'pdfmetaclone') return;
+    if (options.conversionType === 'pdfmetaclone')
+      return;
     for (let f of event.originalEvent.dataTransfer.files) {
       if ($('#SOProcessFolderTickbox').is(':checked')) {
         ipcRenderer.send('convertpdf:drag.folder', f.path);
@@ -196,46 +193,80 @@ $(document).ready(() => {
     switch (currentAction) {
       // PDF metadata clone option
       case 'pdfmetaclone':
-        // Hide and show correct labels, file option
-        $('#stepTwoLabel').removeClass('is-hidden');
-        $('#stepTwoLabel2').addClass('is-hidden');
-        $('#stepTwoButtonLabel').removeClass('is-hidden');
-        $('#stepTwoButtonLabel2').addClass('is-hidden');
-        $('#stepTwoDragLabel').removeClass('is-hidden');
-        $('#stepTwoDragLabel2').addClass('is-hidden');
-        // Hide step 2 partially
-        $('#S2Two').css('display', 'none');
-        $('#S2Three').css('display', 'none');
-        // Show step 3
-        $('#StepThree').removeClass('is-hidden');
-        // Hide process folder tickbox option
-        $('#SOProcessFolderContainer').css('display', 'none');
+        showElementsToPdfClone();
+        break;
+
+      case 'pdf2join':
+        showElementsToPdf2Join();
         break;
 
       default:
-        // Show step 2
-        $('#S2Two').css('display', 'block');
-        $('#S2Three').css('display', 'block');
-        // Hide step 3
-        $('#StepThree').addClass('is-hidden');
-        $('#SOProcessFolderContainer').css('display', 'block');
-        if ($('#SOProcessFolderTickbox').is(':checked')) {
-          // Hide and show correct labels, folder option
-          $('#stepTwoLabel').addClass('is-hidden');
-          $('#stepTwoLabel2').removeClass('is-hidden');
-          $('#stepTwoButtonLabel').addClass('is-hidden');
-          $('#stepTwoButtonLabel2').removeClass('is-hidden');
-          $('#stepTwoDragLabel').addClass('is-hidden');
-          $('#stepTwoDragLabel2').removeClass('is-hidden');
-        } else {
-          // Hide and show correct labels, file option
-          $('#stepTwoLabel').removeClass('is-hidden');
-          $('#stepTwoLabel2').addClass('is-hidden');
-          $('#stepTwoButtonLabel').removeClass('is-hidden');
-          $('#stepTwoButtonLabel2').addClass('is-hidden');
-          $('#stepTwoDragLabel').removeClass('is-hidden');
-          $('#stepTwoDragLabel2').addClass('is-hidden');
-        }
+        showElementsForDefault();
+        break;
+
     }
   });
 });
+
+/*
+  showElementsToPdfClone
+ */
+function showElementsToPdfClone() {
+  // Hide and show correct labels, file option
+  $('#stepTwoLabel').removeClass('is-hidden');
+  $('#stepTwoLabel2').addClass('is-hidden');
+  $('#stepTwoButtonLabel').removeClass('is-hidden');
+  $('#stepTwoButtonLabel2').addClass('is-hidden');
+  $('#stepTwoDragLabel').removeClass('is-hidden');
+  $('#stepTwoDragLabel2').addClass('is-hidden');
+  // Hide step 2 partially
+  $('#S2Two').css('display', 'none');
+  $('#S2Three').css('display', 'none');
+  // Show step 3
+  $('#StepThree').removeClass('is-hidden');
+  // Hide process folder tickbox option
+  $('#SOProcessFolderContainer').css('display', 'none');
+
+  return;
+}
+
+/*
+  showElementsToPdf2Join
+ */
+function showElementsToPdf2Join() {
+  // Hide step 2 partially
+  $('#S2Two').css('display', 'none');
+  $('#S2Three').css('display', 'none');
+  // Hide process folder tickbox option
+  //$('#SOProcessFolderContainer').css('display', 'none');
+  $('#SOProcessFolderTickbox').click();
+}
+
+/*
+  showElementsForDefault
+ */
+function showElementsForDefault() {
+  // Show step 2
+  $('#S2Two').css('display', 'block');
+  $('#S2Three').css('display', 'block');
+  // Hide step 3
+  $('#StepThree').addClass('is-hidden');
+  $('#SOProcessFolderContainer').css('display', 'block');
+  if ($('#SOProcessFolderTickbox').is(':checked')) {
+    // Hide and show correct labels, folder option
+    $('#stepTwoLabel').addClass('is-hidden');
+    $('#stepTwoLabel2').removeClass('is-hidden');
+    $('#stepTwoButtonLabel').addClass('is-hidden');
+    $('#stepTwoButtonLabel2').removeClass('is-hidden');
+    $('#stepTwoDragLabel').addClass('is-hidden');
+    $('#stepTwoDragLabel2').removeClass('is-hidden');
+  } else {
+    // Hide and show correct labels, file option
+    $('#stepTwoLabel').removeClass('is-hidden');
+    $('#stepTwoLabel2').addClass('is-hidden');
+    $('#stepTwoButtonLabel').removeClass('is-hidden');
+    $('#stepTwoButtonLabel2').addClass('is-hidden');
+    $('#stepTwoDragLabel').removeClass('is-hidden');
+    $('#stepTwoDragLabel2').addClass('is-hidden');
+  }
+}
